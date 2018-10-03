27ce8e05ca275ac2043cacd501fea1cab4ee3f76
Sean DuBois <sean@siobud.com>
2018-10-03 01:24:28 -0700

    Init

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	startListener()
+}
+
+func startListener() {
+	pc, err := net.ListenPacket("udp", "127.0.0.1:4444")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer pc.Close()
+
+	buffer := make([]byte, 8192)
+	for {
+		i, _, err := pc.ReadFrom(buffer)
+		if i == 0 || err != nil {
+			panic(fmt.Sprintf("%d %s", i, err.Error()))
+		}
+		fmt.Println(buffer[:i])
+	}
+}
