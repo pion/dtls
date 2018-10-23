f954caabcff6dba8079ed2a175a9508db31cf89a
Sean DuBois <sean@siobud.com>
2018-10-23 00:38:48 -0700

    Only support TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256 for now
    Update instructions to generate ECDSA key (not RSA)

--- README.md
+++ README.md
@@ -3,9 +3,12 @@ Don't depend on this, it will be orphaned/deleted after it lands
 
 # OpenSSL
 ```
-  openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365
-  openssl s_server -cert cert.pem -key key.pem -dtls1 -accept 4444
-  openssl s_client -dtls1 -connect 127.0.0.1:4444 -debug -cert cert.pem -key key.pem
+  openssl ecparam -out key.pem -name prime256v1 -genkey
+  openssl req -new -sha256 -key key.pem -out server.csr
+  openssl x509 -req -sha256 -days 365 -in server.csr -signkey key.pem -out cert.pem
+
+  openssl s_server -dtls1_2 -cert cert.pem -key key.pem -accept 4444
+  openssl s_client -dtls1_2 -connect 127.0.0.1:4444 -debug -cert cert.pem -key key.pem
 ```
 
 # Run the example
--- pkg/dtls/cipher_suite.go
+++ pkg/dtls/cipher_suite.go
@@ -4,22 +4,17 @@ type cipherSuiteID uint16
 
 // Taken from https://www.iana.org/assignments/tls-parameters/tls-parameters.xml
 const (
-	TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA    cipherSuiteID = 0xc009
-	TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256 cipherSuiteID = 0xc023
-)
-
-// A cipherSuite is a specific combination of key agreement, cipher and MAC
+	TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256 cipherSuiteID = 0xc02b
+) // A cipherSuite is a specific combination of key agreement, cipher and MAC
 // function. All cipher suites currently assume RSA key agreement.
 type cipherSuite struct {
 	id cipherSuiteID
 }
 
 var cipherSuites = map[cipherSuiteID]*cipherSuite{
-	TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA:    {id: TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256},
-	TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256: {id: TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA},
+	TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256: {id: TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256},
 }
 
 var defaultCipherSuites = []*cipherSuite{
-	cipherSuites[TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA],
-	cipherSuites[TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256],
+	cipherSuites[TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256],
 }
