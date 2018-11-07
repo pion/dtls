831bf9ac07481bef6535731f30f577ae8b205845
Sean DuBois <sean@siobud.com>
2018-11-07 00:51:05 -0800

    Implement encryptionKey PRF, TLS 1.2 PRFes fully implemented

--- pkg/dtls/prf.go
+++ pkg/dtls/prf.go
@@ -9,6 +9,7 @@ import (
 
 const (
 	prfMasterSecretLabel = "master secret"
+	prfKeyExpansionLabel = "key expansion"
 )
 
 type encryptionKeys struct {
@@ -47,6 +48,31 @@ func prfMasterSecret(preMasterSecret, clientRandom, serverRandom []byte) []byte 
 	return append(p1, p2[:16]...)
 }
 
-func prfEncryptionKeys() (e encryptionKeys) {
-	return e
+func prfEncryptionKeys(masterSecret, clientRandom, serverRandom []byte) *encryptionKeys {
+	hmacSHA256 := func(key, data []byte) []byte {
+		mac := hmac.New(sha256.New, key)
+		mac.Write(data)
+		return mac.Sum(nil)
+	}
+
+	seed := append(append([]byte(prfKeyExpansionLabel), serverRandom...), clientRandom...)
+	a0 := seed
+	a1 := hmacSHA256(masterSecret, a0)
+	a2 := hmacSHA256(masterSecret, a1)
+	a3 := hmacSHA256(masterSecret, a2)
+	a4 := hmacSHA256(masterSecret, a3)
+
+	p1 := hmacSHA256(masterSecret, append(a1, seed...))
+	p2 := hmacSHA256(masterSecret, append(a2, seed...))
+	p3 := hmacSHA256(masterSecret, append(a3, seed...))
+	p4 := hmacSHA256(masterSecret, append(a4, seed...))
+	p := append(append(append(p1, p2...), p3...), p4...)
+	return &encryptionKeys{
+		clientMACKey:   p[:20],
+		serverMACKey:   p[20:40],
+		clientWriteKey: p[40:56],
+		serverWriteKey: p[56:72],
+		clientWriteIV:  p[72:88],
+		serverWriteIV:  p[88:104],
+	}
 }
