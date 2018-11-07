df55431a76a95292e722eeb9b7ea4979666747fd
Sean DuBois <sean@siobud.com>
2018-11-07 00:17:55 -0800

    Implement prf for MasterSecret

--- pkg/dtls/prf.go
+++ pkg/dtls/prf.go
@@ -1,6 +1,15 @@
 package dtls
 
-import "golang.org/x/crypto/curve25519"
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+
+	"golang.org/x/crypto/curve25519"
+)
+
+const (
+	prfMasterSecretLabel = "master secret"
+)
 
 type encryptionKeys struct {
 	clientMACKey   []byte
@@ -22,7 +31,20 @@ func prfPreMasterSecret(publicKey, privateKey [32]byte, curve namedCurve) ([]byt
 }
 
 func prfMasterSecret(preMasterSecret, clientRandom, serverRandom []byte) []byte {
-	return nil
+	hmacSHA256 := func(key, data []byte) []byte {
+		mac := hmac.New(sha256.New, key)
+		mac.Write(data)
+		return mac.Sum(nil)
+	}
+
+	seed := append(append([]byte(prfMasterSecretLabel), clientRandom...), serverRandom...)
+	a0 := seed
+	a1 := hmacSHA256(preMasterSecret, a0)
+	a2 := hmacSHA256(preMasterSecret, a1)
+	p1 := hmacSHA256(preMasterSecret, append(a1, seed...))
+	p2 := hmacSHA256(preMasterSecret, append(a2, seed...))
+
+	return append(p1, p2[:16]...)
 }
 
 func prfEncryptionKeys() (e encryptionKeys) {
