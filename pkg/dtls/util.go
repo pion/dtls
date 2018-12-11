e7b31f29eb9121f2ea5c56f35acafe7fb7e87a9e
Sean DuBois <sean@siobud.com>
2018-12-11 12:18:31 -0800

    Implement E2E test for Client/Server
    Relates to #11

--- pkg/dtls/util.go
+++ pkg/dtls/util.go
@@ -1,7 +1,16 @@
 package dtls
 
 import (
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
 	"encoding/binary"
+	"encoding/hex"
+	"math/big"
+	"time"
 )
 
 // Parse a big endian uint24
@@ -19,4 +28,50 @@ func putBigEndianUint24(out []byte, in uint32) {
 	tmp := make([]byte, 4)
 	binary.BigEndian.PutUint32(tmp, in)
 	copy(out, tmp[1:])
+}
+
+// GenerateSelfSigned creates a self-signed certificate
+func GenerateSelfSigned() (*x509.Certificate, crypto.PrivateKey, error) {
+	priv, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	origin := make([]byte, 16)
+
+	// Max random value, a 130-bits integer, i.e 2^130 - 1
+	maxBigInt := new(big.Int)
+	/* #nosec */
+	maxBigInt.Exp(big.NewInt(2), big.NewInt(130), nil).Sub(maxBigInt, big.NewInt(1))
+	serialNumber, err := rand.Int(rand.Reader, maxBigInt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	template := x509.Certificate{
+		ExtKeyUsage: []x509.ExtKeyUsage{
+			x509.ExtKeyUsageClientAuth,
+			x509.ExtKeyUsageServerAuth,
+		},
+		BasicConstraintsValid: true,
+		NotBefore:             time.Now(),
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		NotAfter:              time.Now().AddDate(0, 1, 0),
+		SerialNumber:          serialNumber,
+		Version:               2,
+		Subject:               pkix.Name{CommonName: hex.EncodeToString(origin)},
+		IsCA:                  true,
+	}
+
+	raw, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	cert, err := x509.ParseCertificate(raw)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return cert, priv, nil
 }
