ae880b38b0ba1ad84e57536de83a63f5359dac5d
backkem <mail@backkem.me>
2018-12-11 23:55:37 +0100

    Add basic transport tests

--- cmd/util.go
+++ cmd/util.go
@@ -2,18 +2,10 @@ package cmd
 
 import (
 	"bufio"
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/hex"
 	"fmt"
 	"io"
-	"math/big"
 	"os"
 	"strings"
-	"time"
 )
 
 const bufSize = 8192
@@ -41,45 +33,6 @@ func Chat(conn io.ReadWriter) {
 		_, err = conn.Write([]byte(text))
 		Check(err)
 	}
-}
-
-// GenerateCertificate is a helper to generate a certificate and private key
-func GenerateCertificate() (*x509.Certificate, *ecdsa.PrivateKey) {
-	priv, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	Check(err)
-
-	origin := make([]byte, 16)
-	Check(err)
-
-	// Max random value, a 130-bits integer, i.e 2^130 - 1
-	maxBigInt := new(big.Int)
-	/* #nosec */
-	maxBigInt.Exp(big.NewInt(2), big.NewInt(130), nil).Sub(maxBigInt, big.NewInt(1))
-	serialNumber, err := rand.Int(rand.Reader, maxBigInt)
-	Check(err)
-
-	template := x509.Certificate{
-		ExtKeyUsage: []x509.ExtKeyUsage{
-			x509.ExtKeyUsageClientAuth,
-			x509.ExtKeyUsageServerAuth,
-		},
-		BasicConstraintsValid: true,
-		NotBefore:             time.Now(),
-		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-		NotAfter:              time.Now().AddDate(0, 1, 0),
-		SerialNumber:          serialNumber,
-		Version:               2,
-		Subject:               pkix.Name{CommonName: hex.EncodeToString(origin)},
-		IsCA:                  true,
-	}
-
-	raw, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
-	Check(err)
-
-	cert, err := x509.ParseCertificate(raw)
-	Check(err)
-
-	return cert, priv
 }
 
 // Check is a helper to throw errors in the examples
