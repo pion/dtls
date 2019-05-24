35e382c4b5c792965dad28392eebf0b12179df91
Aleksandr Razumov <ar@gortc.io>
2019-05-25 00:04:50 +0300

    E2E: Fix back goroutine profiling
    Previous change broke that.

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -14,7 +14,6 @@ import (
 )
 
 const testMessage = "Hello World"
-const baseServerPort = 5555
 const testTimeLimit = 5 * time.Second
 const messageRetry = 200 * time.Millisecond
 
@@ -55,6 +54,24 @@ func simpleReadWrite(errChan chan error, outChan chan string, conn io.ReadWriteC
 	maybePushError(conn.Close())
 }
 
+func pickPort(t testing.TB) int {
+	t.Helper()
+	conn, err := net.ListenPacket("udp4", "127.0.0.1:0")
+	if err != nil {
+		t.Fatalf("failed to pickPort: %v", err)
+	}
+	defer func() {
+		_ = conn.Close()
+	}()
+	switch addr := conn.LocalAddr().(type) {
+	case *net.UDPAddr:
+		return addr.Port
+	default:
+		t.Fatalf("unknown addr type %T", addr)
+		return 0
+	}
+}
+
 /*
   Simple DTLS Client/Server can communicate
     - Assert that you can send messages both ways
@@ -63,8 +80,6 @@ func simpleReadWrite(errChan chan error, outChan chan string, conn io.ReadWriteC
 */
 func TestPionE2ESimple(t *testing.T) {
 	expectedGoRoutineCount := runtime.NumGoroutine()
-	serverPort := baseServerPort
-
 	for _, cipherSuite := range []dtls.CipherSuiteID{
 		dtls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 		dtls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
@@ -74,7 +89,7 @@ func TestPionE2ESimple(t *testing.T) {
 		serverChan := make(chan string)
 		var messageRecvCount uint64 // Counter to make sure both sides got a message
 
-		serverPort++
+		serverPort := pickPort(t)
 
 		cert, key, err := dtls.GenerateSelfSigned()
 		if err != nil {
