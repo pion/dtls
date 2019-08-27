7bfef634899420cfea057aa72c8efc7409cc1696
Daniele Sluijters <daenney@users.noreply.github.com>
2019-08-27 00:08:54 -0700

    Swap remaining cipher suites to pointer receivers
    Due to the previous change in order to introduce locking on cipher
    suites that keep track of their initialization state we update all
    cipher suites to be pointer receivers for consistency.

--- cipher_suite_tls_ecdhe_rsa_with_aes_128_gcm_sha256.go
+++ cipher_suite_tls_ecdhe_rsa_with_aes_128_gcm_sha256.go
@@ -4,14 +4,14 @@ type cipherSuiteTLSEcdheRsaWithAes128GcmSha256 struct {
 	cipherSuiteTLSEcdheEcdsaWithAes128GcmSha256
 }
 
-func (c cipherSuiteTLSEcdheRsaWithAes128GcmSha256) certificateType() clientCertificateType {
+func (c *cipherSuiteTLSEcdheRsaWithAes128GcmSha256) certificateType() clientCertificateType {
 	return clientCertificateTypeRSASign
 }
 
-func (c cipherSuiteTLSEcdheRsaWithAes128GcmSha256) ID() CipherSuiteID {
+func (c *cipherSuiteTLSEcdheRsaWithAes128GcmSha256) ID() CipherSuiteID {
 	return TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256
 }
 
-func (c cipherSuiteTLSEcdheRsaWithAes128GcmSha256) String() string {
+func (c *cipherSuiteTLSEcdheRsaWithAes128GcmSha256) String() string {
 	return "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
 }
--- cipher_suite_tls_ecdhe_rsa_with_aes_256_cbc_sha.go
+++ cipher_suite_tls_ecdhe_rsa_with_aes_256_cbc_sha.go
@@ -4,14 +4,14 @@ type cipherSuiteTLSEcdheRsaWithAes256CbcSha struct {
 	cipherSuiteTLSEcdheEcdsaWithAes256CbcSha
 }
 
-func (c cipherSuiteTLSEcdheRsaWithAes256CbcSha) certificateType() clientCertificateType {
+func (c *cipherSuiteTLSEcdheRsaWithAes256CbcSha) certificateType() clientCertificateType {
 	return clientCertificateTypeRSASign
 }
 
-func (c cipherSuiteTLSEcdheRsaWithAes256CbcSha) ID() CipherSuiteID {
+func (c *cipherSuiteTLSEcdheRsaWithAes256CbcSha) ID() CipherSuiteID {
 	return TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA
 }
 
-func (c cipherSuiteTLSEcdheRsaWithAes256CbcSha) String() string {
+func (c *cipherSuiteTLSEcdheRsaWithAes256CbcSha) String() string {
 	return "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA"
 }
