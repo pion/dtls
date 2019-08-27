e8e0eb34d94ecca1309fb824ce3f40b91f64c5de
Daniele Sluijters <daenney@users.noreply.github.com>
2019-08-27 00:08:54 -0700

    Swap remaining cipher suites to pointer receivers
    Due to the previous change in order to introduce locking on cipher
    suites that keep track of their initialization state we update all
    cipher suites to be pointer receivers for consistency.

--- cipher_suite_tls_psk_with_aes_128_gcm_sha256.go
+++ cipher_suite_tls_psk_with_aes_128_gcm_sha256.go
@@ -4,18 +4,18 @@ type cipherSuiteTLSPskWithAes128GcmSha256 struct {
 	cipherSuiteTLSEcdheEcdsaWithAes128GcmSha256
 }
 
-func (c cipherSuiteTLSPskWithAes128GcmSha256) certificateType() clientCertificateType {
+func (c *cipherSuiteTLSPskWithAes128GcmSha256) certificateType() clientCertificateType {
 	return clientCertificateType(0)
 }
 
-func (c cipherSuiteTLSPskWithAes128GcmSha256) ID() CipherSuiteID {
+func (c *cipherSuiteTLSPskWithAes128GcmSha256) ID() CipherSuiteID {
 	return TLS_PSK_WITH_AES_128_GCM_SHA256
 }
 
-func (c cipherSuiteTLSPskWithAes128GcmSha256) String() string {
+func (c *cipherSuiteTLSPskWithAes128GcmSha256) String() string {
 	return "TLS_PSK_WITH_AES_128_GCM_SHA256"
 }
 
-func (c cipherSuiteTLSPskWithAes128GcmSha256) isPSK() bool {
+func (c *cipherSuiteTLSPskWithAes128GcmSha256) isPSK() bool {
 	return true
 }
