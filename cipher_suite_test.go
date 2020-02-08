a7627d572d93117dd38f8990a9124950eac994bb
Daniele Sluijters <daenney@users.noreply.github.com>
2020-02-08 13:55:52 +0100

    Add CipherSuiteName
    This adds the CiperSuiteName function. It behaves exactly like
    the tls.CipherSuiteName function that's being added for Go 1.14.
    
    Relates to #148

--- cipher_suite_test.go
+++ cipher_suite_test.go
@@ -21,3 +21,20 @@ func TestDecodeCipherSuites(t *testing.T) {
 		// todo: compare result
 	}
 }
+
+func TestCipherSuiteName(t *testing.T) {
+	testCases := []struct {
+		suite    CipherSuiteID
+		expected string
+	}{
+		{TLS_ECDHE_ECDSA_WITH_AES_128_CCM, "TLS_ECDHE_ECDSA_WITH_AES_128_CCM"},
+		{CipherSuiteID(0x0000), "0x0000"},
+	}
+
+	for _, testCase := range testCases {
+		res := CipherSuiteName(testCase.suite)
+		if res != testCase.expected {
+			t.Fatalf("Expected: %s, got %s", testCase.expected, res)
+		}
+	}
+}
