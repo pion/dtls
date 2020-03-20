6f63195df194b98770b8499f39f5ee9fac6067cd
Jeroen de Bruijn <vidavidorra@gmail.com>
2020-03-20 10:53:34 +0100

    Add example with certificate verification
    This example uses certificates from files (in the certificates
    directory), so shows how to use certificates from files and how
    certificates can be verified. This example has both client and server
    verification.

--- examples/util/util.go
+++ examples/util/util.go
@@ -3,10 +3,19 @@ package util
 
 import (
 	"bufio"
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -16,6 +25,7 @@ const bufSize = 8192
 func Chat(conn io.ReadWriter) {
 	go func() {
 		b := make([]byte, bufSize)
+
 		for {
 			n, err := conn.Read(b)
 			Check(err)
@@ -24,12 +34,15 @@ func Chat(conn io.ReadWriter) {
 	}()
 
 	reader := bufio.NewReader(os.Stdin)
+
 	for {
 		text, err := reader.ReadString('\n')
 		Check(err)
+
 		if strings.TrimSpace(text) == "exit" {
 			return
 		}
+
 		_, err = conn.Write([]byte(text))
 		Check(err)
 	}
@@ -44,10 +57,90 @@ func Check(err error) {
 			fmt.Printf("Warning: %v\n", err)
 			return
 		}
+
 		fmt.Printf("net.Error: %v\n", err)
 		panic(err)
 	default:
 		fmt.Printf("error: %v\n", err)
 		panic(err)
 	}
+}
+
+// LoadKeyAndCertificate reads certificates or key from file
+func LoadKeyAndCertificate(keyPath string, certificatePath string) (*tls.Certificate, error) {
+	privateKey, err := LoadKey(keyPath)
+	if err != nil {
+		return nil, err
+	}
+
+	certificate, err := LoadCertificate(certificatePath)
+	if err != nil {
+		return nil, err
+	}
+
+	certificate.PrivateKey = privateKey
+
+	return certificate, nil
+}
+
+// LoadKey Load/read key from file
+func LoadKey(path string) (crypto.PrivateKey, error) {
+	rawData, err := ioutil.ReadFile(filepath.Clean(path))
+	if err != nil {
+		return nil, err
+	}
+
+	block, _ := pem.Decode(rawData)
+	if block == nil || !strings.HasSuffix(block.Type, "PRIVATE KEY") {
+		return nil, errors.New("block is not a private key, unable to load key")
+	}
+
+	if key, err := x509.ParsePKCS1PrivateKey(block.Bytes); err == nil {
+		return key, nil
+	}
+
+	if key, err := x509.ParsePKCS8PrivateKey(block.Bytes); err == nil {
+		switch key := key.(type) {
+		case *rsa.PrivateKey, *ecdsa.PrivateKey:
+			return key, nil
+		default:
+			return nil, errors.New("unknown key time in PKCS#8 wrapping, unable to load key")
+		}
+	}
+
+	if key, err := x509.ParseECPrivateKey(block.Bytes); err == nil {
+		return key, nil
+	}
+
+	return nil, errors.New("no private key found, unable to load key")
+}
+
+// LoadCertificate Load/read certificate(s) from file
+func LoadCertificate(path string) (*tls.Certificate, error) {
+	rawData, err := ioutil.ReadFile(filepath.Clean(path))
+	if err != nil {
+		return nil, err
+	}
+
+	var certificate tls.Certificate
+
+	for {
+		block, rest := pem.Decode(rawData)
+		if block == nil {
+			break
+		}
+
+		if block.Type != "CERTIFICATE" {
+			return nil, errors.New("block is not a certificate, unable to load certificates")
+		}
+
+		certificate.Certificate = append(certificate.Certificate, block.Bytes)
+		rawData = rest
+	}
+
+	if len(certificate.Certificate) == 0 {
+		return nil, errors.New("no certificate found, unable to load certificates")
+	}
+
+	return &certificate, nil
 }
