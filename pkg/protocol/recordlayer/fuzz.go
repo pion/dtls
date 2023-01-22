6aaf97c5cea488373f6dcdca971e52d209e33831
Steffen Vogel <post@steffenvogel.de>
2023-01-22 12:47:48 +0100

    Fix fuzzing of recordLayer
    The old code caused compilation errors
    an was clearly not covered by any tests so far.

--- pkg/protocol/recordlayer/fuzz.go
+++ pkg/protocol/recordlayer/fuzz.go
@@ -1,18 +1,20 @@
 //go:build gofuzz
 // +build gofuzz
 
-package dtls
+package recordlayer
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func partialHeaderMismatch(a, b recordlayer.Header) bool {
+func partialHeaderMismatch(a, b Header) bool {
 	// Ignoring content length for now.
-	a.contentLen = b.contentLen
+	a.ContentLen = b.ContentLen
 	return a != b
 }
 
 func FuzzRecordLayer(data []byte) int {
-	var r recordLayer
+	var r RecordLayer
 	if err := r.Unmarshal(data); err != nil {
 		return 0
 	}
@@ -23,14 +25,14 @@ func FuzzRecordLayer(data []byte) int {
 	if len(buf) == 0 {
 		panic("zero buff") // nolint
 	}
-	var nr recordLayer
+	var nr RecordLayer
 	if err = nr.Unmarshal(data); err != nil {
 		panic(err) // nolint
 	}
-	if partialHeaderMismatch(nr.recordlayer.Header, r.recordlayer.Header) {
+	if partialHeaderMismatch(nr.Header, r.Header) {
 		panic( // nolint
 			fmt.Sprintf("header mismatch: %+v != %+v",
-				nr.recordlayer.Header, r.recordlayer.Header,
+				nr.Header, r.Header,
 			),
 		)
 	}
