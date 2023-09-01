52dc612579d3effd85a911e475ff9bb1a1590dd7
Daniel Mangum <georgedanielmangum@gmail.com>
2023-09-01 14:25:00 -0400

    Correctly identify client and server with PSK ID
    Updates the PSK ID hints on the client and server examples to match the
    type of endpoint they represent.
    
    Signed-off-by: Daniel Mangum <georgedanielmangum@gmail.com>

--- examples/dial/cid/main.go
+++ examples/dial/cid/main.go
@@ -28,7 +28,7 @@ func main() {
 			fmt.Printf("Server's hint: %s \n", hint)
 			return []byte{0xAB, 0xC1, 0x23}, nil
 		},
-		PSKIdentityHint:       []byte("Pion DTLS Server"),
+		PSKIdentityHint:       []byte("Pion DTLS Client"),
 		CipherSuites:          []dtls.CipherSuiteID{dtls.TLS_PSK_WITH_AES_128_CCM_8},
 		ExtendedMasterSecret:  dtls.RequireExtendedMasterSecret,
 		ConnectionIDGenerator: dtls.OnlySendCIDGenerator(),
