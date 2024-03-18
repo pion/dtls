768a023fe2c508a0d5445a5281a47d73f1bd64f1
Jordan Bonecutter <i@jordanbonecutter.com>
2024-03-18 10:19:29 -0700

    test serial behavior to ensure parallel test is valid

--- listener_test.go
+++ listener_test.go
@@ -1,6 +1,7 @@
 package dtls
 
 import (
+	"context"
 	"net"
 	"testing"
 	"time"
@@ -13,6 +14,46 @@ import (
 // The second is normal.
 // We should quickly complete a handshake with the second and start reading.
 func TestParallelHandshakes(t *testing.T) {
+	ln, done := setupHandshakingTest(t)
+	defer ln.Close()
+
+	type AcceptHandshaker interface {
+		AcceptHandshake() (Handshaker, error)
+	}
+
+	for idx := 0; idx < 2; idx++ {
+		hs, err := ln.(AcceptHandshaker).AcceptHandshake()
+		if err != nil {
+			t.Fatalf("Failed accepting: %s", err.Error())
+		}
+		go hs.Handshake()
+	}
+
+	select {
+	case err := <-done:
+		if err != nil {
+			t.Error(err)
+		}
+	case <-time.After(time.Millisecond*100):
+		t.Errorf("Expected second connection to handshake quickly")
+	}
+}
+
+// TestSerialHandshakes tests that Accept uses serial handshakes.
+// This ensures that the parallel handshake test actually proves changed behavior.
+func TestSerialHandshakes(t *testing.T) {
+	ln, _ := setupHandshakingTest(t)
+	defer ln.Close()
+	for idx := 0; idx < 2; idx++ {
+		_, err := ln.Accept()
+		if err == nil {
+			t.Fatalf("Expected to fail accepting with timeout")
+		}
+	}
+}
+
+// setupHandshakingTest creates the setup for a handshaking test.
+func setupHandshakingTest(t *testing.T) (ln net.Listener, done chan error) {
 	lnAddr := &net.UDPAddr{
 		IP: net.IPv4(127, 0, 0, 1),
 		Port: 5284,
@@ -20,6 +61,9 @@ func TestParallelHandshakes(t *testing.T) {
 	config := &Config{
 		PSK: func(b []byte) ([]byte, error) {
 			return []byte("testpsk"), nil
+		},
+		ConnectContextMaker: func() (context.Context, func()) {
+			return context.WithTimeout(context.Background(), time.Second*2)
 		},
 		PSKIdentityHint: []byte("testhint"),
 		CipherSuites:         []CipherSuiteID{TLS_PSK_WITH_AES_128_CCM_8},
@@ -42,31 +86,14 @@ func TestParallelHandshakes(t *testing.T) {
 		t.Fatalf("Failed creating conn: %s", err.Error())
 	}
 
-	done := make(chan error)
+	done = make(chan error)
 	go doTestConn(sleepyConn, lnAddr, config, func(error){})
 	time.Sleep(time.Second)
 	go doTestConn(conn, lnAddr, config, func(err error) {
 		done <- err
 	})
 
-	type AcceptHandshaker interface {
-		AcceptHandshake() (Handshaker, error)
-	}
-
-	hs, err := ln.(AcceptHandshaker).AcceptHandshake()
-	if err != nil {
-		t.Fatalf("Failed accepting: %s", err.Error())
-	}
-	go hs.Handshake()
-
-	select {
-	case err := <-done:
-		if err != nil {
-			t.Error(err)
-		}
-	case <-time.After(time.Millisecond*100):
-		t.Errorf("Expected second connection to handshake quickly")
-	}
+	return ln, done
 }
 
 func doTestConn(
@@ -93,9 +120,4 @@ type sleepy struct {
 func (s sleepy) ReadFrom(p []byte) (n int, add net.Addr, err error) {
 	time.Sleep(time.Hour)
 	return s.PacketConn.ReadFrom(p)
-}
-
-func (s sleepy) WriteTo(p []byte, addr net.Addr) (n int, err error) {
-	time.Sleep(time.Hour)
-	return s.PacketConn.WriteTo(p, addr)
 }
