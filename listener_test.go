7f9e5f36bff3d797ee0b1da721ef58530f4d9ad9
Jordan Bonecutter <i@jordanbonecutter.com>
2024-03-18 10:59:27 -0700

    cleaner

--- listener_test.go
+++ listener_test.go
@@ -14,46 +14,75 @@ import (
 // The second is normal.
 // We should quickly complete a handshake with the second and start reading.
 func TestParallelHandshakes(t *testing.T) {
-	ln, done := setupHandshakingTest(t, 5284)
-	defer ln.Close()
+	done := make(chan error)
+	doHandshakingTest(
+		t, 5284,
+		func(inner net.PacketConn, ln net.Listener, config *Config) {
+			Client(inner, ln.Addr(), config)
+		},
+		func(inner net.PacketConn, ln net.Listener, config *Config) {
+			conn, err := Client(inner, ln.Addr(), config)
+			if err != nil {
+				done <- err
+				return
+			}
 
-	type AcceptHandshaker interface {
-		AcceptHandshake() (Handshaker, error)
-	}
+			conn.Close()
+			done <- nil
+		},
+		func(ln net.Listener) {
+			type AcceptHandshaker interface {
+				AcceptHandshake() (Handshaker, error)
+			}
 
-	for idx := 0; idx < 2; idx++ {
-		hs, err := ln.(AcceptHandshaker).AcceptHandshake()
-		if err != nil {
-			t.Fatalf("Failed accepting: %s", err.Error())
-		}
-		go hs.Handshake()
-	}
+			for idx := 0; idx < 2; idx++ {
+				hs, err := ln.(AcceptHandshaker).AcceptHandshake()
+				if err != nil {
+					t.Fatalf("Failed accepting: %s", err.Error())
+				}
+				go hs.Handshake()
+			}
 
-	select {
-	case err := <-done:
-		if err != nil {
-			t.Error(err)
-		}
-	case <-time.After(time.Millisecond*100):
-		t.Errorf("Expected second connection to handshake quickly")
-	}
+			select {
+			case err := <-done:
+				if err != nil {
+					t.Error(err)
+				}
+			case <-time.After(time.Millisecond*100):
+				t.Errorf("Expected second connection to handshake quickly")
+			}
+		},
+	)
 }
 
 // TestSerialHandshakes tests that Accept uses serial handshakes.
 // This ensures that the parallel handshake test actually proves changed behavior.
 func TestSerialHandshakes(t *testing.T) {
-	ln, _ := setupHandshakingTest(t, 5285)
-	defer ln.Close()
-	for idx := 0; idx < 2; idx++ {
-		_, err := ln.Accept()
-		if err == nil {
-			t.Fatalf("Expected to fail accepting with timeout")
-		}
-	}
+	doHandshakingTest(
+		t, 5284,
+		func(inner net.PacketConn, ln net.Listener, config *Config) {
+			Client(inner, ln.Addr(), config)
+		},
+		func(inner net.PacketConn, ln net.Listener, config *Config) {
+			Client(inner, ln.Addr(), config)
+		},
+		func(ln net.Listener) {
+			_, err := ln.Accept()
+			if err == nil {
+				t.Errorf("Expected to time out, but succeeded")
+			}
+		},
+	)
 }
 
 // setupHandshakingTest creates the setup for a handshaking test.
-func setupHandshakingTest(t *testing.T, port int) (ln net.Listener, done chan error) {
+func doHandshakingTest(
+	t *testing.T,
+	port int,
+	sleepyConnHandler func(conn net.PacketConn, ln net.Listener, config *Config),
+	normalConnHandler func(conn net.PacketConn, ln net.Listener, config *Config),
+	finisher func(ln net.Listener),
+) {
 	lnAddr := &net.UDPAddr{
 		IP: net.IPv4(127, 0, 0, 1),
 		Port: port,
@@ -74,42 +103,23 @@ func setupHandshakingTest(t *testing.T, port int) (ln net.Listener, done chan er
 	if err != nil {
 		t.Fatalf("Failed creating listener: %s", err.Error())
 	}
+	defer ln.Close()
 
 	conn, err := net.ListenUDP("udp4", &net.UDPAddr{})
 	if err != nil {
 		t.Fatalf("Failed creating conn: %s", err.Error())
 	}
 	sleepyConn := sleepy{conn}
+	go sleepyConnHandler(sleepyConn, ln, config)
+	time.Sleep(time.Millisecond * 100)
 
 	conn, err = net.ListenUDP("udp4", &net.UDPAddr{})
 	if err != nil {
 		t.Fatalf("Failed creating conn: %s", err.Error())
 	}
+	go normalConnHandler(conn, ln, config)
+	finisher(ln)
 
-	done = make(chan error)
-	go doTestConn(sleepyConn, lnAddr, config, func(error){})
-	time.Sleep(time.Millisecond * 100)
-	go doTestConn(conn, lnAddr, config, func(err error) {
-		done <- err
-	})
-
-	return ln, done
-}
-
-func doTestConn(
-	inner net.PacketConn,
-	lnAddr net.Addr,
-	config *Config,
-	onDone func(error),
-) {
-	conn, err := Client(inner, lnAddr, config)
-	if err != nil {
-		onDone(err)
-		return
-	}
-
-	conn.Close()
-	onDone(nil)
 	return
 }
 
