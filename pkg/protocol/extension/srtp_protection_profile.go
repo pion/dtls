2f1aa70e5addd1168526ceb044f75bd7a1abd9e9
sirzooro <daniel@poradnik-webmastera.com>
2024-07-20 11:57:35 +0200

    Mark NULL and AES256CM SRTP ciphers as supported
    DTLS server checks this list during handshake. Without this change new
    NULL and AES256CM SRTP ciphers were ignored.

--- pkg/protocol/extension/srtp_protection_profile.go
+++ pkg/protocol/extension/srtp_protection_profile.go
@@ -22,6 +22,10 @@ func srtpProtectionProfiles() map[SRTPProtectionProfile]bool {
 	return map[SRTPProtectionProfile]bool{
 		SRTP_AES128_CM_HMAC_SHA1_80: true,
 		SRTP_AES128_CM_HMAC_SHA1_32: true,
+		SRTP_AES256_CM_SHA1_80:      true,
+		SRTP_AES256_CM_SHA1_32:      true,
+		SRTP_NULL_HMAC_SHA1_80:      true,
+		SRTP_NULL_HMAC_SHA1_32:      true,
 		SRTP_AEAD_AES_128_GCM:       true,
 		SRTP_AEAD_AES_256_GCM:       true,
 	}
