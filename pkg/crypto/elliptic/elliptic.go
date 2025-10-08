e7cbd62208a45412ece0a2d4dd790eab97137b1b
philipch07 <philipch07@gmail.com>
2025-10-08 16:59:34 -0400

    Migrate elliptic curves from elliptic to ecdh

--- pkg/crypto/elliptic/elliptic.go
+++ pkg/crypto/elliptic/elliptic.go
@@ -5,12 +5,10 @@
 package elliptic
 
 import (
-	"crypto/elliptic"
+	"crypto/ecdh"
 	"crypto/rand"
 	"errors"
 	"fmt"
-
-	"golang.org/x/crypto/curve25519"
 )
 
 var errInvalidNamedCurve = errors.New("invalid named curve")
@@ -84,34 +82,36 @@ func Curves() map[Curve]bool {
 }
 
 // GenerateKeypair generates a keypair for the given Curve.
-func GenerateKeypair(c Curve) (*Keypair, error) {
-	switch c { //nolint:revive
-	case X25519:
-		tmp := make([]byte, 32)
-		if _, err := rand.Read(tmp); err != nil {
-			return nil, err
-		}
-
-		var public, private [32]byte
-		copy(private[:], tmp)
-
-		curve25519.ScalarBaseMult(&public, &private)
-
-		return &Keypair{X25519, public[:], private[:]}, nil
-	case P256:
-		return ellipticCurveKeypair(P256, elliptic.P256(), elliptic.P256())
-	case P384:
-		return ellipticCurveKeypair(P384, elliptic.P384(), elliptic.P384())
-	default:
-		return nil, errInvalidNamedCurve
-	}
-}
-
-func ellipticCurveKeypair(nc Curve, c1, c2 elliptic.Curve) (*Keypair, error) {
-	privateKey, x, y, err := elliptic.GenerateKey(c1, rand.Reader) //nolint:staticcheck
+func GenerateKeypair(curve Curve) (*Keypair, error) {
+	ec, err := curve.toECDH()
 	if err != nil {
 		return nil, err
 	}
 
-	return &Keypair{nc, elliptic.Marshal(c2, x, y), privateKey}, nil //nolint:staticcheck
+	sk, err := ec.GenerateKey(rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+
+	pk := sk.PublicKey()
+
+	return &Keypair{
+		Curve:      curve,
+		PublicKey:  pk.Bytes(), // NIST: SEC1 uncompressed (04||X||Y); X25519: 32 bytes
+		PrivateKey: sk.Bytes(), // Scalar suitable for ecdh.NewPrivateKey
+	}, nil
+}
+
+// toECDH returns the crypto/ecdh curve for our enum.
+func (c Curve) toECDH() (ecdh.Curve, error) {
+	switch c {
+	case X25519:
+		return ecdh.X25519(), nil
+	case P256:
+		return ecdh.P256(), nil
+	case P384:
+		return ecdh.P384(), nil
+	default:
+		return nil, errInvalidNamedCurve
+	}
 }
--- pkg/crypto/elliptic/elliptic_test.go
+++ pkg/crypto/elliptic/elliptic_test.go
@@ -4,6 +4,8 @@
 package elliptic
 
 import (
+	crand "crypto/rand"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -26,4 +28,33 @@ func TestString(t *testing.T) {
 			assert.Equal(t, tt.in.String(), tt.out)
 		})
 	}
+}
+
+func TestGenerateKeypair_InvalidCurve(t *testing.T) {
+	var invalid Curve = 0 // not a supported curve
+	_, err := GenerateKeypair(invalid)
+	assert.ErrorIs(t, err, errInvalidNamedCurve)
+}
+
+// create a fake reader that is guaranteed to fail to trigger a failure in generate keypair.
+type failingReader struct{}
+
+func (failingReader) Read(p []byte) (int, error) {
+	return 0, errors.ErrUnsupported // any error is fine here.
+}
+
+func TestGenerateKeypair_RandFailure(t *testing.T) {
+	// replace crypto/rand.Reader to force ecdh.GenerateKey to fail.
+	orig := crand.Reader
+	crand.Reader = failingReader{}
+	defer func() { crand.Reader = orig }()
+
+	_, err := GenerateKeypair(P256)
+	assert.Error(t, err)
+}
+
+func TestToECDH_InvalidCurve(t *testing.T) {
+	var invalid Curve = 0xFFFF
+	_, err := invalid.toECDH()
+	assert.ErrorIs(t, err, errInvalidNamedCurve)
 }
