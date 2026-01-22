01f7ba5fd42a22633c6459e3f0c71ed17eea4b3d
Pion <59523206+pionbot@users.noreply.github.com>
2026-01-22 08:42:10 +0000

    Update CI configs to v0.11.37
    Update lint scripts and CI configs.

--- examples/dial/cid/main.go
+++ examples/dial/cid/main.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-FileCopyrightText: 2026 The Pion community <https://pion.ly>
 // SPDX-License-Identifier: MIT
 
 // Package main implements an example DTLS client using a pre-shared key.
