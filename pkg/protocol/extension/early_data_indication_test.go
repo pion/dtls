c40faa970bb4d40b3ffb3e0a17e7ff67b3a2359d
theodorsm <theodor@midtlien.com>
2026-02-25 19:48:03 +0100

    Add EarlyDataIndication extension

---
+++ pkg/protocol/extension/early_data_indication_test.go
@@ -0,0 +1,73 @@
+// SPDX-FileCopyrightText: 2026 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
+package extension
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEarlyDataIndication_NewSessionTicket(t *testing.T) {
+	earlyData := uint32(128)
+	extension := EarlyDataIndication{MaxEarlyData: &earlyData}
+
+	raw, err := extension.Marshal()
+	assert.NoError(t, err)
+
+	expect := []byte{
+		0x00, 0x2a, // extension type
+		0x00, 0x04, // extension length
+		0x00, 0x00, // MaxEarlyData
+		0x00, 0x80, // MaxEarlyData
+	}
+	assert.Equal(t, expect, raw)
+
+	newExtension := EarlyDataIndication{}
+
+	assert.NoError(t, newExtension.Unmarshal(expect))
+	assert.Equal(t, extension.MaxEarlyData, newExtension.MaxEarlyData)
+}
+
+func TestEarlyDataIndication_CHEE(t *testing.T) {
+	extension := EarlyDataIndication{}
+
+	raw, err := extension.Marshal()
+	assert.NoError(t, err)
+
+	expect := []byte{
+		0x00, 0x2a, // extension type
+		0x00, 0x00, // extension length
+	}
+	assert.Equal(t, expect, raw)
+
+	newExtension := EarlyDataIndication{}
+
+	assert.NoError(t, newExtension.Unmarshal(expect))
+	assert.Nil(t, newExtension.MaxEarlyData)
+}
+
+func FuzzEarlyDataIndicationUnmarshal(f *testing.F) {
+	testCases := [][]byte{
+		// NewSessionTicket
+		{
+			0x00, 0x2a, // extension type
+			0x00, 0x04, // extension length
+			0x00, 0x00, // MaxEarlyData
+			0x00, 0x80, // MaxEarlyData
+		},
+		// ClientHello, EncryptedExtensions
+		{
+			0x00, 0x2a, // extension type
+			0x00, 0x00, // extension length
+		},
+	}
+	for _, tc := range testCases {
+		f.Add(tc)
+	}
+
+	f.Fuzz(func(_ *testing.T, data []byte) {
+		_ = (&EarlyDataIndication{}).Unmarshal(data)
+	})
+}
