b219ca9b30dcacdea98082bcc1716e1575621ec0
theodorsm <theodor@midtlien.com>
2026-03-17 16:56:03 +0100

    Add ServerName fuzz case

--- pkg/protocol/extension/server_name_test.go
+++ pkg/protocol/extension/server_name_test.go
@@ -19,3 +19,20 @@ func TestServerName(t *testing.T) {
 	assert.NoError(t, newExtension.Unmarshal(raw))
 	assert.Equal(t, extension.ServerName, newExtension.ServerName)
 }
+
+func FuzzServerNameUnmarshal(f *testing.F) {
+	tc := []byte{
+		0x0, 0x0, 0x0, 0x10, 0x0, 0xe, 0x0, 0x0, 0xb, 0x74, 0x65, 0x73, 0x74,
+		0x2e, 0x64, 0x6f, 0x6d, 0x61, 0x69, 0x6e,
+	}
+	f.Add(tc)
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		sn := ServerName{}
+		err := sn.Unmarshal(data)
+		if err != nil {
+			return
+		}
+		testExtDataLength(t, &sn, data, false)
+	})
+}
