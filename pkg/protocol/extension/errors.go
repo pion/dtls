d0e736c2e6bcd89dada06355ef95fd0fe3957d14
Theodor Midtlien <theodor@midtlien.com>
2026-03-24 17:36:45 +0100

    DTLS 1.3 OID filter extension (#814)

--- pkg/protocol/extension/errors.go
+++ pkg/protocol/extension/errors.go
@@ -74,4 +74,13 @@ var (
 	errInvalidCertificateAuthFormat = &protocol.FatalError{
 		Err: errors.New("invalid Certificate Authorities extension format"), //nolint:err113
 	}
+	errEmptyOIDFilter = &protocol.InternalError{
+		Err: errors.New("no oid set for a OID filter"), //nolint:err113
+	}
+	errOIDFiltersFormat = &protocol.FatalError{
+		Err: errors.New("invalid OID filters extension format"), //nolint:err113
+	}
+	errDuplicateOID = &protocol.FatalError{
+		Err: errors.New("duplicate OID filters"), //nolint:err113
+	}
 )
