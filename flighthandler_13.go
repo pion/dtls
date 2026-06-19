61a241fcab546f7f43699039fff1257c038ee680
Theodor Midtlien <theodor@midtlien.com>
2026-06-19 17:20:01 +0200

    DTLS 1.3 FSM architecture (#738)
    * Add WIP arch and FSM for DTLS 1.3
    
    * Add version to state
    
    * Fix review
    
    * Fix lint
    
    * Remove compatibility mode
    
    DTLS implementations do not use the TLS 1.3 "compatibility mode"
    described in Appendix D.4 of TLS13.
    
    * Rework version negotiation, add flight 1 gen
    
    * Add dual-stack tests
    
    * Fix client dual-stack
    
    * Add TODO for onFLightState13 callback

---
+++ flighthandler_13.go
@@ -0,0 +1,37 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
+package dtls
+
+import (
+	"context"
+
+	"github.com/pion/dtls/v3/pkg/protocol/alert"
+)
+
+// Parse received handshakes and return next flightVal.
+type flightParser13 func( //nolint:unused
+	context.Context,
+	flightConn,
+	*State,
+	*handshakeCache,
+	*handshakeConfig,
+) (flightVal13, *alert.Alert, error)
+
+//nolint:unused
+type flightGenerator13 func(flightConn, *State, *handshakeCache, *handshakeConfig) ([]*packet, *alert.Alert, error)
+
+//nolint:unused
+func (f flightVal13) getFlightParser13() (flightParser13, error) {
+	return nil, errFlightUnimplemented13
+}
+
+//nolint:unused
+func (f flightVal13) getFlightGenerator13() (gen flightGenerator13, retransmit bool, err error) {
+	switch f {
+	case flight13_1:
+		return flight13_1Generate, true, nil
+	default:
+		return nil, false, errFlightUnimplemented13
+	}
+}
