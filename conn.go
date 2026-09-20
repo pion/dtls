7f6751cffd54c5f21ab9cc74d43eeb7a891669b5
Jo Turk <git@joturk.net>
2026-09-20 13:17:46 +0300

    Remove doc file

--- conn.go
+++ conn.go
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2026 The Pion community <https://pion.ly>
 // SPDX-License-Identifier: MIT
 
+// Package dtls implements Datagram Transport Layer Security (DTLS) 1.2 and 1.3.
 package dtls
 
 import (
